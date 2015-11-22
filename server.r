608ac488464298b6d01eb6cb5b3351b8e74861d2
pyarepiyush <pyare_piyush@hotmail.com>
2015-11-21 20:58:52 -0800

    UI and Server shiny codes for the Course project

--- server.r
+++ server.r
@@ -1,14 +1,34 @@
+library(shiny)
+
+diabetesRisk <- function(glucose) glucose / 200
+x = c(1,2,3)
+
 
 shinyServer(
-  function(input, output) {
-    output$text1 <- renderText({input$text1})
-    output$text2 <- renderText({input$text2})
-    output$text3 <- renderText({
-      output$text3 <- renderText({
-        if (input$goButton == 0) "You have not pressed the button"
-        else if (input$goButton == 1) "you pressed it once"
-        else "OK quit pressing it"
-      })
-    })
-  }
-)
+  function(input,output) {
+  
+  input1 <- reactive({c(as.numeric(input$glucose1),as.numeric(input$glucose2),as.numeric(input$glucose3)   )})
+             
+
+  prediction = reactive( {as.numeric( diabetesRisk(input1()) ) } )
+  
+  output$dataset <- renderPrint({data.frame(InputValues= input1(), Prediction = prediction(), row.names = c("Day1:", "Day2:", "Day3:")      )})  
+    
+  output$inputValues <- renderPrint({paste( "Day1:", input$glucose1, 
+                                            "Day2:", input$glucose2, 
+                                            "Day3:", input$glucose3 )}) 
+  output$prediction = renderPrint({prediction() })
+
+    
+  output$plots <- renderPlot({
+    par(mfrow = c(1, 1))
+  
+    plot(x, prediction() ,type="l", col="red", xaxt='n', xlab = "Day", ylab="Prediction of Diabetes based on the Glucose mg/dl")
+    axis(1,x)
+    
+  })
+  
+  
+                    
+  
+})
--- ui.R
+++ ui.R
@@ -1,17 +1,31 @@
+library(shiny)
+shinyUI(
+  pageWithSidebar(
+    # Application title
+    headerPanel("Diabetes prediction"        
+                
+                ),
+    
+    sidebarPanel(
+      
+      p('The application "Diabetes Prediction" helps you predict the risk of diabetes given the amount of blood glucose level for 3 consecutive days.'),
+      p(' '),
+    
+      p('Enter the 3-day glucose reading in mg/dl'),
+      numericInput('glucose1', 'Glucose mg/dl day1:', 90, min = 50, max = 200, step = 5),
+      numericInput('glucose2', 'Glucose mg/dl day2:', 90, min = 50, max = 200, step = 5),
+      numericInput('glucose3', 'Glucose mg/dl day3:', 90, min = 50, max = 200, step = 5),
+      submitButton('Submit')
+    ),
+    mainPanel(
+      h3('Results of prediction'),
+      p('Table showing the Values you entered, and the corresponding prediction for Diabetes for those days.'),
+      p('The predicted values is between 0 and 1(higher number indicates that the probability is high'),
+      verbatimTextOutput("dataset"),
 
-shinyUI(pageWithSidebar(
-  headerPanel("Hello Shiny!"),
-  sidebarPanel(
-    textInput(inputId="text1", label = "Input Text1"),
-    textInput(inputId="text2", label = "Input Text2"),
-    actionButton("goButton", "Go!")
-  ),
-  mainPanel(
-    p('Output text1'),
-    textOutput('text1'),
-    p('Output text2'),
-    textOutput('text2'),
-    p('Output text3'),
-    textOutput('text3')
+      plotOutput('plots'),
+      p('Plot showing the Prediction for 3 days. ')
+      
+    )
   )
-))
+)
